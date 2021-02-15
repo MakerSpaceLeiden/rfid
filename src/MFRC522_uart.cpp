06054fc57e74eb49cc3fb5e8b8674a7c54a13830
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2021-02-14 22:18:02 -0300

    Added comments to clarify the code

--- src/MFRC522_uart.cpp
+++ src/MFRC522_uart.cpp
@@ -1,3 +1,11 @@
+/*
+ * According to datasheet https://www.nxp.com/docs/en/data-sheet/MFRC522.pdf
+ * on section "8.1.1 Automatic microcontroller interface detection" to use the
+ * UART interface pins EA (32) and I2C (1) must be connected to GND.
+ * The board I use comes with SPI interface selected so I had to manually connect pin
+ * EA to ground.
+ */
+
 #include <Arduino.h>
 #include <MFRC522.h>
 
@@ -19,9 +27,13 @@ bool MFRC522_UART::PCD_Init() {
 }
 
 void MFRC522_UART::PCD_WriteRegister(MFRC522::PCD_Register reg, byte value) {
+	// Mask used for writing to registers. See section "8.1.3.3 UART framing" from datasheet.
 	_serialPort.write(0x7F & reg);
+	// Waits for confirmation
   	while (!_serialPort.available());
+	// Removes confirmation byte from input buffer
   	_serialPort.read();
+	// Writes data
 	_serialPort.write(value);
 }
 
@@ -33,8 +45,11 @@ void MFRC522_UART::PCD_WriteRegister(
 }
 
 byte MFRC522_UART::PCD_ReadRegister(MFRC522::PCD_Register reg) {
+	// Mask used for reading from registers. See section "8.1.3.3 UART framing" from datasheet.
 	_serialPort.write(0x80 | reg);
+	// Waits for input
 	while (!_serialPort.available());
+	// Reads data
 	byte value = _serialPort.read();
 	return value;
 }
@@ -43,14 +58,17 @@ void MFRC522_UART::PCD_ReadRegister(
   	MFRC522::PCD_Register reg, byte count, byte *values, byte rxAlign) {
 
 	byte index = 0;
+	// Only update bit positions rxAlign..7 in values[0]
 	if (rxAlign) {
 		byte mask = 0xFF << rxAlign;
 		byte value = this->PCD_ReadRegister(reg);
+		// Apply mask to both current value of values[0] and the new data in value.
 		values[0] = (values[0] & ~mask) | (value & mask);
 		++index;
 	}
 
 	while (index < count) {
+		// Keeps reading from the same address
 		values[index] = this->PCD_ReadRegister(reg);
 		++index;
 	}
